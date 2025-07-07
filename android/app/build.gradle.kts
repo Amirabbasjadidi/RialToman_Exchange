1e314dc2bc24b7744a4ba2ce473c5514037643a8
Amirabbas Jadidi <86429722+Amirabbasjadidi@users.noreply.github.com>
2025-07-07 12:52:45 +0330

    Add Flutter version

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "ir.amirabbasjadidi.rialtoman"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "ir.amirabbasjadidi.rialtoman"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/ir/amirabbasjadidi/rialtoman/MainActivity.kt
@@ -0,0 +1,5 @@
+package ir.amirabbasjadidi.rialtoman
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
